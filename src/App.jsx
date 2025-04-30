eeaff84d18f0be4fdfbafc44c8e3f1c0a1c131f8
MBashov <galacticos.mb@gmail.com>
2025-04-30 22:15:11 +0200

    Add Quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import StartSqreen from "./components/StartScreen"
+import { useState } from 'react'
+
+import StartSqreen from "./components/start-screen/StartScreen"
+import Quiz from './components/quiz/Quiz';
 
 function App() {
+    const [hasStarted, setHasStarted] = useState(false);
 
-  return (
-    <StartSqreen />
-  )
+    const handleStart = () => setHasStarted(true);
+
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center p-4">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+
+                {!hasStarted ? (<StartSqreen onStart={handleStart} />)
+                    : (<Quiz />)
+                }
+
+            </div>
+
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/quiz/Quiz.jsx
@@ -0,0 +1,15 @@
+export default function Quiz() {
+    return (
+        <div>
+            <h2 className="text-xl font-semibold mb-4">
+                Question 1 of 10
+            </h2>
+            <p className="mb-4 text-gray-800">
+                What language runs in a web browser?
+            </p>
+            <div className="space-y-2">
+                Java, Python, C++, JavaScript
+            </div>
+        </div>
+    );
+}
