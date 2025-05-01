f47ac2bcdcd5f450cf7d0327af392fadbce0ace7
MBashov <galacticos.mb@gmail.com>
2025-05-01 20:02:51 +0200

    Add handle answer logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,29 @@ import { useState } from 'react'
 
 import StartSqreen from "./components/start-screen/StartScreen"
 import Quiz from './components/quiz/Quiz';
+import { quizData } from './data/qusetions';
 
 function App() {
     const [hasStarted, setHasStarted] = useState(false);
 
+    const [current, setCurrent] = useState(0);
+    const [score, setScore] = useState(0);
+
     const handleStart = () => setHasStarted(true);
+
+    const handleAnsewr = (selected) => {
+
+        const correct = quizData[current].answer;
+        if (correct === selected) {
+            setScore((s) => s + 1);
+        }
+
+        const next = current + 1;
+
+        if (next < quizData.length) {
+            setCurrent(next);
+        }
+    }
 
 
     return (
@@ -14,7 +32,14 @@ function App() {
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
 
                 {!hasStarted ? (<StartSqreen onStart={handleStart} />)
-                    : (<Quiz />)
+                    : (
+                        <Quiz
+                            questionData={quizData[current]}
+                            questionIndex={current}
+                            totalQuestions={quizData.length}
+                            onAnswer={handleAnsewr}
+                        />
+                    )
                 }
 
             </div>
